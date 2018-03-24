38666ac9c54885a22c3d5e1886a6dd904628740e
EC2 Default User <ec2-user@ip-172-31-29-207.us-west-2.compute.internal>
2018-03-24 23:09:23 +0000

    Sends texts

--- aws/app.js
+++ aws/app.js
@@ -1,6 +1,24 @@
+const accountSid = 'ACc4772fd12e4a53426ae51b0308a74ff9'
+const authToken = '189aadc721a07385b8d561181ded0fa7'
+const client = require('twilio')(accountSid, authToken)
 const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+const notificationOpts = {
+ toBinding: JSON.stringify({
+   binding_type: 'sms',
+   address: '+18573660630',
+ }),
+ body: 'Nick has a server working like a boss!',
+};
+
+client.notify
+ .services('IS8b7d6977600f5d6e17219bf9e6057e86')
+ .notifications.create(notificationOpts)
+ .then(notification => res.send(notification.sid))
+ .catch(error => res.send(error));
+
 })
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
