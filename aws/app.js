febafaa81c6a233d081c69b14bbf717ba460a604
EC2 Default User <ec2-user@ip-172-31-29-207.us-west-2.compute.internal>
2018-03-25 17:53:23 +0000

    Can change number, some rate limiting

--- aws/app.js
+++ aws/app.js
@@ -4,21 +4,45 @@ const client = require('twilio')(accountSid, authToken)
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+let number = '+18573660630'
+let sent = false
 
+app.get('/notify/', (req, res) => {
+
+if (sent === true) {
+	res.send("Not going to spam")
+	return
+}
+
+if (req.query.number != undefined) {
+	number = '+1'+req.query.number
+}
+console.log(number)
 const notificationOpts = {
  toBinding: JSON.stringify({
    binding_type: 'sms',
-   address: '+18573660630',
+   address: number,
  }),
- body: 'Nick has a server working like a boss!',
+ body: 'Something smells a bit ripe...',
 };
+
+sent = true
 
 client.notify
  .services('IS8b7d6977600f5d6e17219bf9e6057e86')
  .notifications.create(notificationOpts)
  .then(notification => res.send(notification.sid))
- .catch(error => res.send(error));
+ .catch(error => res.send(error))
 
 })
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.get('/change/', (req, res) => {
+	if(req.query.number == undefined){
+		res.send("'number' must be included")
+		return
+	}
+	number = '+1' + req.query.number
+	res.send('New number set')
+	sent = false
+})
+app.listen(3000, () => console.log('App listening on port 3000'))
